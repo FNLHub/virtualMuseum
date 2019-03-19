444e4c550d40fef0c4e8e513d07e0fe150ec927a
John Redden <jtredden@gmail.com>
2019-03-18 19:27:09 -0700

    Viewer Work
    Changed to hand.

--- public/scripts/viewerScript.js
+++ public/scripts/viewerScript.js
@@ -1,3 +1,12 @@
+
+
+//objs
+var handObjURL = 'https://firebasestorage.googleapis.com/v0/b/fnlvirtualmuseum.appspot.com/o/3dObjects%2Fhand%2Fhandcomplete.obj?alt=media&token=5cf5ba2e-6bba-4eca-86f9-6646f9a090bd';
+var handMtlURL = 'https://firebasestorage.googleapis.com/v0/b/fnlvirtualmuseum.appspot.com/o/3dObjects%2Fhand%2Fhandcomplete.mtl?alt=media&token=a6a1454a-7415-4124-97e1-c9f3141c1fc2';
+var handJpgURL = 'https://firebasestorage.googleapis.com/v0/b/fnlvirtualmuseum.appspot.com/o/3dObjects%2Fhand%2Fhand3.jpg?alt=media&token=f72056d9-d83c-460d-84fb-ab4c8f294746';
+
+
+
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 1000 );
@@ -7,8 +16,11 @@ camera.position.x = 30;
 camera.position.y = 30;
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight);
+//var scriptElement = document.querySelector(".progress");
+//document.body.insertBefore(renderer.domElement, scriptElement);
 document.body.appendChild( renderer.domElement );
+//scriptElement.appendChild( renderer.domElement );
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -35,16 +47,16 @@ scene.add(light);
 var mtlLoader = new THREE.MTLLoader();
 mtlLoader.setTexturePath('assets/');
 mtlLoader.setPath('assets/');
-mtlLoader.load('jawsmaller.mtl', function (materials) {
+mtlLoader.load('handcomplete.mtl', function (materials) {
     
     materials.preload();
 
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
     objLoader.setPath('assets/');
-    objLoader.load('jawsmaller.obj', function (object) {
+    objLoader.load('handcomplete.obj', function (object) {
 
-        $("#progress").hide();
+       $("#progress").hide();
 
         scene.add(object);
         //object.position.y;
