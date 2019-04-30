84b070f6978b9d98775f6c214478cf018ef0759c
John Redden <jtredden@gmail.com>
2019-04-29 20:55:35 -0700

    Saved Database Firestore Entry
    Saved an entry to firestore database then updated the URLs to the files.  Not too bad.

--- public/js/ViewerAdminUpload.js
+++ public/js/ViewerAdminUpload.js
@@ -24,34 +24,53 @@ $(document).ready(function () {
         $("#textureLoading").show();
         $("#objectLoading").show();
 
-        //Save files to Storage firt
-        firebase.storage().ref().child('Uploads/3dObjects/' + objectFile.name).put(objectFile).then(function (fileSnapshot) {
-            // Now get the download URL for this object
-            fileSnapshot.ref.getDownloadURL().then(function (url) {
-                //turn off the loading bar
-                $("#objectLoading").hide();
-                $("#objectIcon").html("done");
-                console.log("objectURL:" + url);
 
-                //now save url to the firebase database (firestore)
-                //TODO save to database
+        // Add a new document with a generated id.
+        firebase.firestore().collection("scanUploads").add({
+            name: objectFile.name,
+            description: "Some description here.",
+            category: "Some Category",
+            objectURL: "",
+            textureURL: ""
+        }).then(function (docRef) { //Save to database established
+            console.log("Document written with ID: ", docRef.id);
 
+            //Now save the files to storage with this docRef.id in URL
+            firebase.storage().ref().child('Uploads/3dObjects/' + docRef.id + "/" + objectFile.name).put(objectFile).then(function (fileSnapshot) {
+                // Now get the download URL for this object
+                fileSnapshot.ref.getDownloadURL().then(function (url) {
+                    //turn off the loading bar
+                    $("#objectLoading").hide();
+                    $("#objectIcon").html("done");
+                    console.log("objectURL:" + url);
 
+                    //now save url to the firebase database (firestore)
+                    firebase.firestore().collection("scanUploads").doc(docRef.id).update({
+                        objectURL: url
+                    });
+
+                });
             });
-        });
-        firebase.storage().ref().child('Uploads/3dTextures/' + textureFile.name).put(textureFile).then(function (fileSnapshot) {
-            //now get the download URL for this texture
-            fileSnapshot.ref.getDownloadURL().then(function (url) {
-                //turn off the loading bar
-                $("#textureLoading").hide();
-                $("#textureIcon").html("done");
-                console.log("textureURL:" + url);
+            firebase.storage().ref().child('Uploads/3dTextures/' + docRef.id + "/" + textureFile.name).put(textureFile).then(function (fileSnapshot) {
+                //now get the download URL for this texture
+                fileSnapshot.ref.getDownloadURL().then(function (url) {
+                    //turn off the loading bar
+                    $("#textureLoading").hide();
+                    $("#textureIcon").html("done");
+                    console.log("textureURL:" + url);
 
-                //now save url to the firebase database (firestore)
-                //TODO save to database
+                    //now save url to the firebase database (firestore)
+                    firebase.firestore().collection("scanUploads").doc(docRef.id).update({
+                        textureURL: url
+                    });
 
+                });
             });
+
+        }).catch(function (error) {
+            console.error("Error adding document: ", error);
         });
+
 
     });
 
